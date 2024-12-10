923409bc66f7f0bc5e776a499724b816170e784a
eder64ghjhj <166666766+eder64ghjhj@users.noreply.github.com>
2024-12-10 15:49:22 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React,{ useState } from 'react'
+import BuscaDeFilmes from './components/BuscaDeFilmes'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <BuscaDeFilmes/>
+    </>
+  )
+}
+
+export default App
